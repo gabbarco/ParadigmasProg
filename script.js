ff7c161e6ae1f8279c63f30e53a62d1bf58d95ec
Gabriel <gabborges37@gmail.com>
2023-03-21 23:19:14 -0300

    Pokemon 1.0

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-let url = "https://pokemon.danielpimentel.com.br/v1/pokemon/geracao/1/50/1";
-		fetch(url)
-			.then((res)=>res.json())
-			.then((res)=>searchPokemons(res.pokemon))
-			.catch((err_)=>console.log(err_));
+let btn = document.getElementById("searchPokemon");
+let pokeName = document.getElementById("name");
+let pokeGen = document.getElementById("gen");
+let result = document.getElementById("result");
 
-           
-function showPokemons(arr,pokemon){
-	let pokedex = document.getElementById("pokedex");
-	let html = "";
+btn.addEventListener('click', checkValidity);
 
-	for(let i=0; i<arr.length; ++i){
-		const nome = arr[i].nome.charAt(0).toUpperCase() + arr[i].nome.slice(1);
-		nome[0] = nome[0].toUpperCase();
-		html += `
-			<div id="wrapper">
-				<img src="${arr[i].img_3d}" height=150 />
-				<div>${nome}</div>
-			</div>
-		`;
+function checkValidity(){
+	let gen = pokeGen.value;
 
-		pokedex.innerHTML = html;
-	}
+	searchPokemon()
+	.then((pokemon)=>{
+		if(pokemon==undefined){
+			alert("Não existe pokemon com esse nome");
+			return;
+		}
+
+		if(pokemon.geracao==gen){
+			pokemon.nome = pokemon.nome.charAt(0).toUpperCase()+pokemon.nome.slice(1);
+
+			let html = `
+				<div><img src="${pokemon.img}"\\></div>
+				<div>
+					<div>Número: ${pokemon.numero}</div>
+					<div>Nome: ${pokemon.nome}</div>
+					<div>Tipo: ${pokemon.tipo}</div>
+					<div>Geração: ${pokemon.geracao}</div>
+				</div>
+			`
+
+			result.innerHTML = html;
+		}
+		else
+			alert("O nome do pokemon não condiz com a geração selecionada");
+	})
+	.catch((err_)=>alert(err_));
 }
 
-function searchPokemons(arr) {
-    let pokedex = document.getElementById("pokedex");
-    let pokemon = document.getElementById("pokemon").value;
-    let geracao = document.getElementById("geracao").value;
-    for(let i=0; i<arr.length; ++i){
-        const nome = arr[i].nome.charAt(0).toUpperCase() + arr[i].nome.slice(1);
-        nome[0] = nome[0].toUpperCase();
-        if (nome === pokemon) {
-            console.log("oi")
-        }
-    }
-    // let s = new Promise((res,rej) => {
+function searchPokemon(){
+	let name = pokeName.value.toLowerCase();
 
-    // })
+	return new Promise((res, rej)=>{
+		fetch(`https://pokemon.danielpimentel.com.br/v1/pokemon/nome/${name}`)
+				.then((poke)=>poke.json())
+				.then((pokemon)=>res(pokemon.pokemon))
+				.catch(()=>rej("Erro de comunicação com a API."));
+	});
 }
