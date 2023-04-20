cd16df70ae5618987dcb1505f0b3deb77aeb3219
Gabriel Barco <gabborges37@gmail.com>
2023-04-20 16:55:52 -0300

    Dimensionamento de Imagem

--- ContornoChromaKey/scripts.js
+++ ContornoChromaKey/scripts.js
@@ -1,44 +1,118 @@
-function gerarContorno() {
-    // Obter a imagem de chroma key
-    const imagem = document.getElementById('imagem');
+let canvasOriginal = document.getElementById("imagemOriginal");
+let ctxOriginal = canvasOriginal.getContext("2d");
 
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = imagem.width;
-    canvas.height = imagem.height;
-    ctx.drawImage(imagem, 0, 0);
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
+let canvasNovo = document.getElementById("imagemNova");
+let ctxNovo = canvasNovo.getContext("2d");
 
-    // Cor do chroma key (verde) em RGBA
-    const verdeR = 0;
-    const verdeG = 255;
-    const verdeB = 0;
-    const verdeA = 255;
+let image = new Image();
+image.src = "imagemChromaKey.png";
 
-    // Cor do contorno em RGBA
-    const contornoR = 255;
-    const contornoG = 0;
-    const contornoB = 0;
-    const contornoA = 255;
+image.onload = function() {
+  canvasOriginal.width = image.width;
+  canvasOriginal.height = image.height;
+  canvasNovo.width = image.width;
+  canvasNovo.height = image.height;
+  ctxOriginal.drawImage(image, 0, 0);
+};
 
-    // Definir o contorno
-    for (let i = 0; i < data.length; i += 4) {
-      if (data[i] === verdeR && data[i + 1] === verdeG && data[i + 2] === verdeB && data[i + 3] === verdeA) {
-        data[i] = contornoR;
-        data[i + 1] = contornoG;
-        data[i + 2] = contornoB;
-        data[i + 3] = contornoA;
-      }
+function horizontal() {
+        ctxNovo.clearRect(0, 0, canvasNovo.width, canvasNovo.height);
+        let imageData = ctxOriginal.getImageData(0, 0, canvasOriginal.width, canvasOriginal.height);
+        let pixels = imageData.data;
+        for (let y = 0; y < canvasOriginal.height; y++) {
+            for (let x = 0; x < canvasOriginal.width / 2; x++) {
+                let i = (y * canvasOriginal.width + x) * 4;
+                let r = pixels[i];
+                let g = pixels[i + 1];
+                let b = pixels[i + 2];
+                let a = pixels[i + 3];
+                let horizontal = (y * canvasOriginal.width + (canvasOriginal.width - x - 1)) * 4;
+                pixels[i] = pixels[horizontal];
+                pixels[i + 1] = pixels[horizontal + 1];
+                pixels[i + 2] = pixels[horizontal + 2];
+                pixels[i + 3] = pixels[horizontal + 3];
+                pixels[horizontal] = r;
+                pixels[horizontal + 1] = g;
+                pixels[horizontal + 2] = b;
+                pixels[horizontal + 3] = a;
+            }
+        }
+        ctxNovo.putImageData(imageData, 0, 0);
+}
+
+    function vertical() {
+        ctxNovo.clearRect(0, 0, canvasNovo.width, canvasNovo.height);
+        let imageData = ctxOriginal.getImageData(0, 0, canvasOriginal.width, canvasOriginal.height);
+        let pixels = imageData.data;
+        for (let y = 0; y < canvasOriginal.height / 2; y++) {
+            for (let x = 0; x < canvasOriginal.width; x++) {
+                let i = (y * canvasOriginal.width + x) * 4;
+                let r = pixels[i];
+                let g = pixels[i + 1];
+                let b = pixels[i + 2];
+                let a = pixels[i + 3];
+                let vertical = ((canvasOriginal.height - y - 1) * canvasOriginal.width + x) * 4;
+                pixels[i] = pixels[vertical];
+                pixels[i + 1] = pixels[vertical + 1];
+                pixels[i + 2] = pixels[vertical + 2];
+                pixels[i + 3] = pixels[vertical + 3];
+                pixels[vertical] = r;
+                pixels[vertical + 1] = g;
+                pixels[vertical + 2] = b;
+                pixels[vertical + 3] = a;
+            }
+        }
+        ctxNovo.putImageData(imageData, 0, 0);
     }
 
-    // Desenhar a imagem com o contorno no elemento de canvas
-    ctx.putImageData(imageData, 0, 0);
-    console.log('Finalizado')
+    function rotacionar() {
+        ctxNovo.clearRect(0, 0, canvasNovo.width, canvasNovo.height);
+        let imageData = ctxOriginal.getImageData(0, 0, canvasOriginal.width, canvasOriginal.height);
+        let pixels = imageData.data;
+        //Criação nova imagem
+        let newImageData = ctxNovo.createImageData(imageData.height, imageData.width);
+        let newPixels = newImageData.data;
 
-    // Substituir a imagem
-    const novaImagem = new Image();
-    novaImagem.src = canvas.toDataURL();
-    novaImagem.alt = 'Imagem com Contorno';
-    imagem.parentNode.replaceChild(novaImagem, imagem);
-  }
+        for (let i = 0; i < pixels.length; i += 4) {
+          let r = pixels[i];
+          let g = pixels[i+1];
+          let b = pixels[i+2];
+          let a = pixels[i+3];
+
+          let x = (i/4) % imageData.width;
+          let y = Math.floor(i / (imageData.width * 4));
+
+          const novoX = y;
+          const novoY = imageData.width - x - 1;
+          const novoI = (novoX + novoY * imageData.height) * 4;
+          
+          newPixels[novoI] = r;
+          newPixels[novoI+1] = g;
+          newPixels[novoI+2] = b;
+          newPixels[novoI+3] = a;
+        }
+        canvasNovo.width = imageData.height;
+        canvasNovo.height = imageData.width;
+        ctxNovo.putImageData(newImageData, 0, 0);
+}
+
+  function reduzir() {
+        ctxNovo.clearRect(0, 0, canvasNovo.width, canvasNovo.height);
+        let imageData = ctxOriginal.getImageData(0, 0, canvasOriginal.width, canvasOriginal.height);
+        let pixels = imageData.data;
+        canvasNovo.width = canvasOriginal.width / 2;
+        canvasNovo.height = canvasOriginal.height / 2;
+        ctxNovo.clearRect(0, 0, canvasNovo.width, canvasNovo.height);
+
+        for (let x = 0; x < canvasNovo.width; x++) {
+          for (let y = 0; y < canvasNovo.height; y++) {
+            let novoX = x * 2;
+            let novoY = y * 2;
+
+            let i = ((novoY * canvasOriginal.width) + novoX) * 4;
+
+            ctxNovo.fillStyle = "rgba(" + pixels[i] + "," + pixels[i + 1] + "," + pixels[i + 2] + "," + pixels[i + 3] + ")";
+            ctxNovo.fillRect(x, y, 1, 1);
+          }
+        }
+}
